cd61e95429a0a30cb11ac0f629c726f454a26c0e
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2024-08-22 10:50:16 +0700

    remove old redundant

--- redirect.js
+++
@@ -1,3 +0,0 @@
-if (!window.location.pathname.endsWith('/') && !window.location.pathname.endsWith('.html')) {
-    window.location.replace(window.location.pathname + '/' + window.location.search + window.location.hash);
-  }
